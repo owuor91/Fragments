5c7f1f9e7da8a1bf992015e85143239cc9948f26
owuor91 <owuor91@gmail.com>
2015-05-03 17:50:46 +0300

    Fragment change method implemented

--- app/src/main/java/com/example/teeshirt/fragments/MainActivity.java
+++ app/src/main/java/com/example/teeshirt/fragments/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.teeshirt.fragments;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -15,25 +18,22 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+   public void changeFragment(View view){
+       Fragment fragment;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+       if (view== findViewById(R.id.button)){
+            fragment = new Fragment1();
+           FragmentManager fm = getFragmentManager();
+           FragmentTransaction ft = fm.beginTransaction();
+           ft.replace(R.id.fragmentholder, fragment);
+           ft.commit();
+       }
+       if (view == findViewById(R.id.button2)){
+           fragment = new Fragment2();
+           FragmentManager fm = getFragmentManager();
+           FragmentTransaction ft = fm.beginTransaction();
+           ft.replace(R.id.fragmentholder,fragment);
+           ft.commit();
+       }
+   }
 }
